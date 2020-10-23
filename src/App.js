009d401abda8773ac4366f7f90466a10c33134da
Rayssa Ferreira <rayssaferr98@gmail.com>
2020-10-23 11:58:16 -0300

    juntei os componentes na home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
+import Localizacao from "./components/Localizacao"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header/>
+      <div>
+        Bem-vindo a nossa clínica
+      </div>
+      <Localizacao/>
+      <Footer/>
     </>
   )
 }
