1fe3515e00b337e78e8f5cd3725c734f842f7aad
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-10-23 15:08:02 -0300

    Updates

---
+++ precache-manifest.3b98d622b3a683a7af74759d98771ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadca8fcb18126804e68dfe512a1853b",
+    "url": "/odontoClinica_React/index.html"
+  },
+  {
+    "revision": "b84d3c8fac8598e5f11d",
+    "url": "/odontoClinica_React/static/css/main.fff8f04e.chunk.css"
+  },
+  {
+    "revision": "ffdee2222ce355a7a9eb",
+    "url": "/odontoClinica_React/static/js/2.c734646a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/odontoClinica_React/static/js/2.c734646a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84d3c8fac8598e5f11d",
+    "url": "/odontoClinica_React/static/js/main.0e89f07a.chunk.js"
+  },
+  {
+    "revision": "a4bf43156864c006e47a",
+    "url": "/odontoClinica_React/static/js/runtime-main.9efca2b6.js"
+  }
+]);
